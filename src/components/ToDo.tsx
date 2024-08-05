12bc7a34b70b5e2d88908666850d9d7eed10db6c
marcus pedersen <marcus@mpedersen.me>
2024-08-05 16:52:41 +0200

    Added local state for ToDo but need to implement a callback function to communicate this to the App component that holds the array of todos.

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { IToDo } from "../interfaces";
 import styles from "../css/ToDo.module.css";
 
@@ -7,13 +7,24 @@ interface IToDoProps {
 }
 
 export default function ToDo( {todo}: IToDoProps): ReactElement {
-  const { title, isDone } = todo;
+  const [isDone, setIsDone] = useState<boolean>(false)
+  const { title } = todo;
+
+  const handleOnClick = (): void => {
+    isDone ? setIsDone(false) : setIsDone(true); 
+  }
+
   return (
-    <>
       <article className={styles.todo}>
         <h2 className={styles.todoTitle}>ToDo: {title}</h2>
-        <p className={styles.todoStatus}>Status: {isDone ? "Done" : "Not Done"}</p>
-      </article>
-    </>
+        <div className={styles.todoStatusAndControl}>
+          <p className={styles.todoStatus}>Status: {isDone ? "Done" : "Not Done"}</p>
+          <span className={styles.checkboxIcon}>
+            <span className="material-symbols-outlined" onClick={handleOnClick}>
+               {isDone ? 'check_box' : 'check_box_outline_blank'}
+            </span>
+          </span>
+        </div>
+     </article>
   )
 }
