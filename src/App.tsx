bcffb3d9866ebe85ca35b95ffc6aa56162f1a2f2
marcus pedersen <marcus@mpedersen.me>
2024-08-05 16:08:46 +0200

    Implement add ToDo feature: handle new ToDo input, submission, and reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { ReactElement, useState } from "react";
+import AddToDo from "./components/AddToDo";
 import ToDoList from "./components/ToDoList";
 import { ToDos } from "./data";
+import { IToDo } from "./interfaces";
 
 import "./app.css"
 
-export function App() {
+export function App(): ReactElement {
+  const [todos, setTodos] = useState<IToDo[]>(ToDos);
+
+  const handleAddToDo = (todoTitle: string) => {
+    setTodos(prevTodos => [...prevTodos, {title: todoTitle, isDone: false}])
+  }
+
   return (
     <div className="App">
-      <ToDoList todos={ToDos}/>
+      <AddToDo onAddToDo={handleAddToDo}/>
+      <ToDoList todos={todos}/>
     </div>
   );
 }
--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -1,7 +1,41 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
-export default function AddToDo(): ReactElement {
+interface IAddToDoProps {
+  onAddToDo: (newTodo: string) => void;
+}
+
+export default function AddToDo( {onAddToDo}: IAddToDoProps ): ReactElement {
+  const [todoTitle, setTodoTitle] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onAddToDo(todoTitle);
+    resetTodoInput();
+  }
+
+  const resetTodoInput = () => {
+    setTodoTitle("");
+  }
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoTitle(e.target.value)
+  }
+
   return (
-    
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Add a new ToDo</legend>
+        <label htmlFor="todoTitle">ToDo</label>
+        <input 
+          type="text" 
+          id="todoTitle"
+          name="title"
+          value={todoTitle}
+          onChange={(e) => handleTitleChange(e)}
+          required
+        />
+        <button type="submit">Add ToDo</button>
+      </fieldset>
+    </form>
   )
 }
